210934e2da3dd8b3f3c2258e6d897bda8b76210f
Rohit <tprohit9@gmail.com>
2021-08-17 04:09:58 +0530

    added submission data

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const tableBodyCollege = document.getElementById("tableBodyCollege");
 const tableBodyDistrict = document.getElementById("tableBodyDistrict");
 
+const submitted = document.getElementById("submitted");
 const registered = document.getElementById("registered");
 const pending = document.getElementById("pending");
 const total = document.getElementById("total");
 const gender = document.getElementById("gender");
 
-function generateCollegeRow(rank, college, registrations)
+function generateCollegeRow(rank, college, registrations, submissions)
 {
     return (
         `<tr>
-            <th scope="row">${rank}</th>
-            <td>${college}</td>
-            <td>${registrations}</td>
+            <th scope="row">${rank ?? -1}</th>
+            <td>${college || "Unknown"}</td>
+            <td>${registrations ?? 0}</td>
+            <td>${submissions ?? 0}</td>
         </tr>`
     );
 }
@@ -21,8 +23,8 @@ function generateDistrictRow(district, registrations)
 {
     return (
         `<tr>
-            <td>${district}</td>
-            <td>${registrations}</td>
+            <td>${district || "Unknown"}</td>
+            <td>${registrations ?? 0}</td>
         </tr>`
     );
 }
@@ -37,7 +39,8 @@ async function getCampusData()
     let rank = 1;
 
     tableBodyCollege.innerHTML = "";
-    data.colleges.forEach(({ college, count }) => tableBodyCollege.innerHTML += generateCollegeRow(rank++, college, count));
+    data.colleges.forEach(({ college, count, submissions }) =>
+        tableBodyCollege.innerHTML += generateCollegeRow(rank++, college, count, submissions));
 }
 
 async function getDistrictData(data) 
@@ -52,6 +55,7 @@ async function getTotalData()
 
     const data = await fetch(url).then(res => res.json());
 
+    submitted.innerHTML = data.submissions;
     registered.innerText = data.done;
     pending.innerText = data.pending;
     gender.innerText = `${Number(100 * data.male / data.total).toFixed(0)}% / ${Number(data.female * 100 / data.total).toFixed(0)}%`;
